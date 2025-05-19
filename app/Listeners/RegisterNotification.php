fc70950ea0b61af217ef819a4a4bb5c3ab152708
Bukola689 <jbukola95@gmail.com>
2025-05-19 16:50:00 +0100

    register listener

--- app/Listeners/RegisterNotification.php
+++ app/Listeners/RegisterNotification.php
@@ -4,18 +4,21 @@ namespace App\Listeners;
 
 use App\Events\UserRegister;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 use Illuminate\Queue\InteractsWithQueue;
 
 class RegisterNotification
 {
+    private $user;
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -26,6 +29,6 @@ class RegisterNotification
      */
     public function handle(UserRegister $event)
     {
-        //
+         Mail::to($event->user)->send(new RegisteredMail($event->user));
     }
 }
