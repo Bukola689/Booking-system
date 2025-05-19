2154b8147c1c13643d164656c620745c6e6ef080
Bukola689 <jbukola95@gmail.com>
2025-05-19 12:43:51 +0100

    login user

--- app/Http/Controllers/V1/Api/LoginController.php
+++ app/Http/Controllers/V1/Api/LoginController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\V1\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+      public function login(Request $request)
+    {
+        $user = User::first();
+
+        $data = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+      $user = User::where('email', $data['email'])->first();
+
+      if(!$user || !Hash::check($data['password'], $user->password))
+      {
+          return response(['message'=>'invalid credentials'], 401);
+      } else {
+        $token  = $user->createToken('myapptoken')->plainTextToken;
+
+        $when = Carbon::now()->addSeconds(10);
+
+        $user->notify((new RegisterNotification($user))->delay($when));
+
+        event(new Registered($user));
+
+        $response = [
+            'user'=>$user,
+            'token'=>$token,
+        ];
+
+        //Cache::put('user');
+
+        return response($response, 200);
+      }
+    }
 }
