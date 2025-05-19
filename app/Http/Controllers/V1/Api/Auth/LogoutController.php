6ee1c29e27d780a995ed31da785f9ba3f128519b
Bukola689 <jbukola95@gmail.com>
2025-05-19 12:45:01 +0100

    logout as a  user

--- app/Http/Controllers/V1/Api/Auth/LogoutController.php
+++ app/Http/Controllers/V1/Api/Auth/LogoutController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\V1\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    //
+      public function logout(Request $request, User $user) 
+    {
+        $user->tokens()->delete();
+
+        return response()->json('You Have Successfully logged out');
+    }
 }
